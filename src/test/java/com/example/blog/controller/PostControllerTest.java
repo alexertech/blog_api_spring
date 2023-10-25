3e707e41a9ca23a6ec613b457384543d84fe1eb8
Alex B <ab@alexertech.com>
2023-10-25 13:31:32 -0300

    Adding new test scenarios for the controller

--- src/test/java/com/example/blog/controller/PostControllerTest.java
+++ src/test/java/com/example/blog/controller/PostControllerTest.java
@@ -12,13 +12,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.mockito.Mockito.*;
 
 import java.util.Optional;
 
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -60,6 +60,60 @@ class PostControllerTest {
         when(postRepository.findById(1L)).thenReturn(Optional.of(post));
 
         mockMvc.perform(get("/posts/1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void createPost_emptyTitle_returnsBadRequest() throws Exception {
+        Post post = new Post();
+        post.setTitle("");
+        post.setBody("Test Body");
+
+        mockMvc.perform(post("/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(post)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void createPost_emptyBody_returnsBadRequest() throws Exception {
+        Post post = new Post();
+        post.setTitle("Test Title");
+        post.setBody("");
+
+        mockMvc.perform(post("/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(post)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void updatePost_returnsOk() throws Exception {
+        Post post = new Post();
+        post.setId(1L);
+        post.setTitle("Updated Title");
+        post.setBody("Updated Body");
+
+        when(postRepository.findById(1L)).thenReturn(Optional.of(post));
+        when(postRepository.save(post)).thenReturn(post);
+
+        mockMvc.perform(put("/posts/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(post)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void deletePost_returnsOk() throws Exception {
+        Post post = new Post();
+        post.setId(1L);
+        post.setTitle("Test Title");
+        post.setBody("Test Body");
+
+        when(postRepository.findById(1L)).thenReturn(Optional.of(post));
+        doNothing().when(postRepository).delete(post);
+
+        mockMvc.perform(delete("/posts/1"))
                 .andExpect(status().isOk());
     }
 }
